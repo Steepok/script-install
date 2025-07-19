5f34a9ae0710fedcf8d59d540b439dfd126f8396
Steepok <megamix496@gmail.com>
2025-07-19 17:13:53 +0000

    arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Ускоряем загрузку пакетов
+sed -i 's/^ParallelDownloads = 5/ParallelDownloads = 15/' /etc/pacman.conf
+
 # Синхронизация времени"
 timedatectl set-ntp true
 
@@ -28,11 +31,8 @@ mount /dev/vda1 /mnt/boot/efi
 mkdir /mnt/home
 mount /dev/vda4 /mnt/home
 
-# Ускоряем загрузку
-sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 15/' /etc/pacman.conf
-
 # Установка базовой системы
-pacstrap -K /mnt base base-devel linux linux-firmware networkmanager sudo vim git grub efibootmgr wget
+pacstrap -K /mnt base base-devel linux linux-headers linux-firmware networkmanager sudo vim git grub efibootmgr wget
 
 # Генерация fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -42,6 +42,12 @@ cat > /mnt/in-chroot.sh <<"EOF"
 
 #!/bin/bash
 set -e
+
+# Обновление системы
+pacman -Syu --noconfirm
+
+# Установка шрифтов
+pacman -S --noconfirm ttf-jetbrains-mono-nerd ttf-firacode-nerd ttf-hack-nerd
 
 # Локализация
 sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
@@ -61,6 +67,10 @@ cat >> /etc/hosts <<EOL
 127.0.1.1	computer.localdomain	computer
 EOL
 
+# Консольная русская раскладка
+echo "KEYMAP=ru" > /etc/vconsole.conf
+echo "FONT=cyr-sun16" >> /etc/vconsole.conf
+
 # Пользователь и пароли
 useradd -mG wheel user
 echo "user:123099" | chpasswd
@@ -77,6 +87,9 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable dbus-broker
 systemctl enable systemd-timesyncd
+
+# Полезные пакеты
+pacman -S --noconfirm kitty dolphin firefox
 
 EOF
 
