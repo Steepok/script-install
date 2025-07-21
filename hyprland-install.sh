56f9e44fe5c1676ddb91725bc221c09cca7d5b02
Steepok <megamix496@gmail.com>
2025-07-21 12:42:20 +0000

    post-install.sh

--- hyprland-install.sh
+++ hyprland-install.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 set -e
 
+#sudo EDITOR=vim visudo
+#123099 #Пароль
+#%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/pacman
+
 # Обновление системы
 sudo pacman -Syu --noconfirm
-
-# Установка служб и утилит
-sudo pacman -S --noconfirm \
-  bluez bluez-utils blueman \
-  pipewire pipewire-audio pipewire-pulse pipewire-alsa pipewire-jack wireplumber pavucontrol \
-  wl-clipboard grim slurp swappy \
-  xdg-user-dirs \
-  dbus xdg-desktop-portal xdg-desktop-portal-hyprland \
-  thunar thunar-archive-plugin file-roller \
-  kitty firefox \
-  qt5-wayland qt6-wayland \
-  papirus-icon-theme ttf-jetbrains-mono ttf-font-awesome \
-  htop curl wget zip unzip man-db man-pages \
-  libvirt qemu-guest-agent \
-  swww \
-  wayland-utils \
-  noto-fonts noto-fonts-cjk noto-fonts-emoji \
-  brightnessctl playerctl
-
-# Установка Hyprland
-sudo pacman -S --noconfirm \
-  hyprland hyprpaper hyprlock waybar wofi rofi
-
-# Включение служб
-sudo systemctl enable --now NetworkManager
-sudo systemctl enable --now dbus-broker
-sudo systemctl enable --now bluetooth
-sudo systemctl enable --now qemu-guest-agent
-sudo systemctl --user enable --now pipewire.service pipewire-pulse.service wireplumber.service
 
 # Создание директорий пользователя
 xdg-user-dirs-update
 
-echo "==> Установка завершена"
+# Установка служб и утилит
+sudo pacman -S --noconfirm hyprland
+
+# Репозиторий AUR и yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd
+
+# Устаеовка tor-browser
+wget https://www.torproject.org/dist/torbrowser/14.5.4/tor-browser-linux-x86_64-14.5.4.tar.xz
+tar -xvf tor-browser-linux-x86_64-14.5.4.tar.xz
+rm tor-browser-linux-x86_64-14.5.4.tar.xz
+
